e638ce39697d943c781ee4406bfde74f6bbadccc
Sarde <samadhan.sarde@accenture.com>
2018-10-03 15:24:50 +0530

    Lazy loading issue is fixed

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -14,7 +14,7 @@ export const appRoutes: Routes = [
     { path: 'events', component: EventsListComponent, resolve: { events: EventListResolver}},
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
     { path: 'events/session/new', component: CreateSessionComponent},
+    { path: 'user', loadChildren: './user/user.module#UserModule'},
     { path: '404', component:Error404Component},
     { path: '', redirectTo: '/events', pathMatch: 'full'}, 
-    { path: 'user', loadChildren: './app/user/user.module#UserModule'}
 ]
